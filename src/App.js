b170f9550842156324325ae83ec632d949e9f23b
chandanyadavbbt <chandan.yadav@blackbasil.tech>
2024-05-29 17:49:49 +0530

    adding multiple props

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
-// src/TextToSpeech.js
-import React, { useState } from 'react';
+import React from 'react'
+import Test from './component/Test'
 
-const App = () => {
-  const [text, setText] = useState('');
-
-  const speak = (text) => {
-    if ('speechSynthesis' in window) {
-      const speech = new SpeechSynthesisUtterance('hello how are you ');
-      window.speechSynthesis.speak(speech);
-    } else {
-      alert('Sorry, your browser does not support text to speech!');
-    }
-  };
-
-  const handleInputChange = (event) => {
-    setText(event.target.value);
-  };
-
-  const handleSpeakClick = () => {
-    speak(text);
-  };
-
+function App() {
   return (
     <div>
-      <h1>Text to Speech</h1>
-      <textarea
-        value={text}
-        onChange={handleInputChange}
-        rows="4"
-        cols="50"
-        placeholder="Enter text here..."
-      />
-      <br />
-      <button onClick={handleSpeakClick}>Speak</button>
+      <Test title={"hello"}/>
+      <Test title={"mello"}/>
+      <Test title={"chello"}/>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
